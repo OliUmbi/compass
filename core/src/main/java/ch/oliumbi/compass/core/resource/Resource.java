d36bdf590aa9f02e2c237fc732e1b4433635dbd1
oliUmbi <oliver.umbricht@hispeed.ch>
2024-01-14 18:24:25 +0100

    added workflow

--- core/src/main/java/ch/oliumbi/compass/core/resource/Resource.java
+++ core/src/main/java/ch/oliumbi/compass/core/resource/Resource.java
@@ -1,6 +1,7 @@
 package ch.oliumbi.compass.core.resource;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,31 +9,15 @@ import java.util.Optional;
 
 public class Resource {
 
-  public static Optional<File> load(String path) {
-    try {
-      ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-      URL resource = contextClassLoader.getResource(path);
-
-      if (resource == null) {
-        return Optional.empty();
-      }
-
-      return Optional.of(new File(resource.toURI()));
-    } catch (Exception ignored) {
-      return Optional.empty();
-    }
-  }
-
   public static Optional<byte[]> loadBytes(String path) {
     try {
-      ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-      URL resource = contextClassLoader.getResource(path);
+      InputStream inputStream = ClassLoader.getSystemResourceAsStream(path);
 
-      if (resource == null) {
+      if (inputStream == null) {
         return Optional.empty();
       }
 
-      return Optional.of(Files.readAllBytes(Path.of(resource.toURI())));
+      return Optional.of(inputStream.readAllBytes());
     } catch (Exception ignored) {
       return Optional.empty();
     }
